0811171753ed838ab295a53e56e83bce29af6cfe
samz00 <43385583+samz00@users.noreply.github.com>
2018-09-18 13:46:59 -0400

    Add files via upload

---
+++ model.m
@@ -0,0 +1,158 @@
+function varargout = model(varargin)
+% MODEL MATLAB code for model.fig
+%      MODEL, by itself, creates a new MODEL or raises the existing
+%      singleton*.
+%
+%      H = MODEL returns the handle to a new MODEL or the handle to
+%      the existing singleton*.
+%
+%      MODEL('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MODEL.M with the given input arguments.
+%
+%      MODEL('Property','Value',...) creates a new MODEL or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before model_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to model_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help model
+
+% Last Modified by GUIDE v2.5 17-Sep-2018 20:46:06
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @model_OpeningFcn, ...
+                   'gui_OutputFcn',  @model_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before model is made visible.
+function model_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to model (see VARARGIN)
+
+% Choose default command line output for model
+handles.output = hObject;
+handles.current = 'eband';
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes model wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = model_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on selection change in examine_type.
+function examine_type_Callback(hObject, eventdata, handles)
+% hObject    handle to examine_type (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+str = get(hObject, 'String');
+val = get(hObject, 'Value');
+command = char(str(val));
+switch command
+    case 'eband'
+        handles.current = 'eband';
+    case 'carrier'
+        handles.current = 'carrier';
+end
+guidata(hObject, handles);
+
+
+
+% Hints: contents = cellstr(get(hObject,'String')) returns examine_type contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from examine_type
+
+
+% --- Executes during object creation, after setting all properties.
+function examine_type_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to examine_type (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in examine.
+function examine_Callback(hObject, eventdata, handles)
+% hObject    handle to examine (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global x0
+A_examine(x0,handles.current,'range',[0,2])
+
+
+% --- Executes on button press in build.
+function build_Callback(hObject, eventdata, handles)
+% hObject    handle to build (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global object
+global x0
+x0 = A_build(object);
+
+
+
+
+function edit_object_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_object (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global object
+
+object = get(handles.edit_object, 'String');
+guidata(hObject, handles);
+
+
+
+
+% Hints: get(hObject,'String') returns contents of edit_object as text
+%        str2double(get(hObject,'String')) returns contents of edit_object as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit_object_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_object (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
