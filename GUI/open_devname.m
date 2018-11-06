726b294d5f68000015ad90a1d0fd87fa4f875230
du153 <42986518+du153@users.noreply.github.com>
2018-11-06 14:04:28 -0500

    Update open_devname

--- GUI/open_devname.m
+++ GUI/open_devname.m
@@ -2,7 +2,7 @@ function hBuildMenu = open_devname(devObj)
 
 %device = Adept;
 
-hBuildMenu = figure('Visible', 'off', 'Position', ...
+hDevNameMenu = figure('Visible', 'off', 'Position', ...
                 [945, 460, 570, 365], 'Name', 'Build Menu', 'MenuBar', ...
                 'none', 'ToolBar', 'none');
            
@@ -11,9 +11,9 @@ prompt = {'Enter the name of the device'};
 title = 'Device Name';
 
 
-answer = inputdlg(prompt,title,[1 40]);
+answer = insputdlg(prompt,title,[1 40]);
 devObj.input_file = answer;
 
-openBuildMenu(devObj);
+hBuildMenu = openBuildMenu(devObj, 1);
 
 end
